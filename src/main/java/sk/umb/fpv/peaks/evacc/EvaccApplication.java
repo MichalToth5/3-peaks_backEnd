b9e0f57017ac76a3f9f3b398b1769d6f65a22615
aulinka <laura.mikusova27@gmail.com>
2022-03-28 14:45:22 +0200

    zaklad aplikacie

---
+++ src/main/java/sk/umb/fpv/peaks/evacc/EvaccApplication.java
@@ -0,0 +1,13 @@
+package sk.umb.fpv.peaks.evacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/umb/fpv/peaks/evacc/EvaccApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.umb.fpv.peaks.evacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
